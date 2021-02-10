81579d794a6568c2b719321ee6c5b019e5fa1185
X-San <ytbaconboi95@gmail.com>
2021-02-10 20:33:58 +0000

    ok so there's numbers now.
    yeah...

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const debugging = require('./debugging.js');
-
-const token = {
-	LET: 0,
-	CONST: 1
-}
-
+let dbg
 function debug(a) {
-	//console.log(a);
+	if (debug)console.log(a);
 }
+
 class ParseNode {
 	children = []
 	start = 0
@@ -19,6 +14,45 @@ class ParseNode {
 	}
 	static tryMatch(src, p) {
 
+	}
+}
+
+class IdentifierReference extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let c = Identifier.tryMatch(src,p)
+		if (c) {
+			return new IdentifierReference(c.start,c.end,[c])
+		}
+	}
+}
+
+class Identifier extends ParseNode {
+	static tryMatch(src, p) {
+		let idn = IdentifierName.tryMatch(src, p)
+		if (idn) {
+			let n = new Identifier(idn.start,idn.end)
+			n.children.push(idn)
+			return n
+		}
+	}
+}
+
+class PrimaryExpression extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let bt = p.pos;
+		let c;
+		if (p.test('this')) {
+			return new PrimaryExpression(bt, p.pos)
+		}
+		p.goto(bt)
+		if (c = IdentifierReference.tryMatch(src,y,a,p)) {
+			return new PrimaryExpression(c.start,c.end,[c])
+		}
+		p.goto(bt)
+		if (c = Literal.tryMatch(src,p)) {
+			return new PrimaryExpression(c.start,c.end,[c])
+		}
+		p.goto(bt)
 	}
 }
 
@@ -52,6 +86,7 @@ class StatementList extends ParseNode {
 		let loops = 0;
 		while (cflag) {
 			let i =  StatementListItem.tryMatch(src, y, a, r, p)
+			debug()
 			if (i) {
 				items.push(i)
 			}
@@ -60,32 +95,21 @@ class StatementList extends ParseNode {
 			}
 		}
 		if (items.length>0) {
-			let n = new StatementList(items[0].start, items[items.length-1].end)
-			n.children.push(...items)
+			let n = new StatementList(items[0].start, items[items.length-1].end, items)
 			return n
 		}
 	}
 }
 class StatementListItem extends ParseNode {
 	static tryMatch(src, y, a, r, p) {
+		p.consumews()
 		// todo add statements
 		let d = Declaration.tryMatch(src,y,a,p)
+		if (!d) d = Statement.tryMatch(src,y,a,r,p)
 		if (d) {
 			let n = new StatementListItem(d.start,d.end);
 			n.children.push(d)
 			return n
-		}
-	}
-}
-
-class Declaration extends ParseNode {
-	static tryMatch(src, y, a, p) {
-		// todo other expressions.
-		let ld = LexicalDeclaration.tryMatch(src,1,y,a,p)
-		if (ld) {
-			let d = new Declaration(ld.start, ld.end)
-			d.children.push(ld)
-			return d
 		}
 	}
 }
@@ -371,18 +395,14 @@ class MemberExpression extends ParseNode {
 	}
 }
 
-class PrimaryExpression extends ParseNode {
-	static tryMatch(src,y,a,p) {
-		let l = Literal.tryMatch(src,p)
-		if (l) {
-			return new PrimaryExpression(l.start,l.end,[l])
-		}
-	}
-}
-
 class Literal extends ParseNode {
 	static tryMatch(src, p) {
+		let bt=p.pos
 		let s = StringLiteral.tryMatch(src,p)
+		if (!s) {
+			p.goto(bt)
+			s = NumericLiteral.tryMatch(src,p)
+		}
 		if (s) {
 			return new Literal(s.start, s.end, [s])
 		}
@@ -427,7 +447,57 @@ class SingleStringCharacters extends ParseNode {
 		}
 	}
 }
-
+class NumericLiteral extends ParseNode {
+	static tryMatch(src,p) {
+		let n = DecimalLiteral.tryMatch(src,p);
+		if (n) return new NumericLiteral(n.start,n.end,[n]);
+	}
+}
+class DecimalLiteral extends ParseNode {
+	static tryMatch(src,p) {
+		let n = DecimalIntegerLiteral.tryMatch(src,p);
+		if (n) return new DecimalLiteral(n.start,n.end,[n]);
+	}
+}
+class DecimalIntegerLiteral extends ParseNode {
+	static tryMatch(src,p) {
+		p.consumews();
+		if (p.get()=='0') return new DecimalIntegerLiteral(p.pos-1,p.pos)
+		else {
+			let n = [];
+			let a = NonZeroDigit.tryMatch(src,p);
+			if (!a) return
+		}
+	}
+}
+class NonZeroDigit extends ParseNode{
+	static tryMatch(src,p) {
+		let bt = p.pos
+		let n =  p.get()
+		if (n=='1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9') return new NonZeroDigit(bt,p.pos)
+	}
+}
+class DecimalDigits extends ParseNode {
+	static tryMatch(src,p) {
+		let f = 1
+		let d = []
+		while (f) {
+			let a = DecimalDigit.tryMatch(src,p)
+			if (!a) {
+				f=0
+				return new DecimalDigits(d[0].start,d[d.length-1].end,d)
+			}
+			d.push(a)
+		}
+	}
+}
+class DecimalDigit extends ParseNode {
+	static tryMatch(src,p) {
+		let bt = p.pos
+		let n =  p.get()
+		if (n=='1'||'2'||'3'||'4'||'5'||'6'||'7'||'8'||'9'||'0') return new DecimalDigit(bt,p.pos)		
+	}
+}
 class SingleStringCharacter extends ParseNode {
 	static tryMatch(src,p) {
 		let c = p.get()
@@ -437,21 +507,15 @@ class SingleStringCharacter extends ParseNode {
 		}
 	}
 }
+
 class BindingIdentifier extends ParseNode {
 	static tryMatch(src, y, a, p) {
 		let id = Identifier.tryMatch(src, p)
-		let n = new BindingIdentifier(id.start,id.end)
-		n.children.push(id)
-		return n
-	}
-}
-
-class Identifier extends ParseNode {
-	static tryMatch(src, p) {
-		let idn = IdentifierName.tryMatch(src, p)
-		let n = new Identifier(idn.start,idn.end)
-		n.children.push(idn)
-		return n
+		if (id) {
+			let n = new BindingIdentifier(id.start,id.end)
+			n.children.push(id)
+			return n
+		}
 	}
 }
 
@@ -516,6 +580,195 @@ class UnicodeIDContinue extends ParseNode {
 		return undefined
 	}
 }
+
+class Statement extends ParseNode {
+	static tryMatch(src,y,a,r,p) {
+		let s = ExpressionStatement.tryMatch(src,y,a,p)
+		if (!s) s = ReturnStatement.tryMatch(src,y,a,p)
+		if (s) return new Statement(s.start,s.end,[s])
+	}
+}
+
+class ExpressionStatement extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		if (p.test('{',0)||p.test('function',0)||p.test('async function',0)||p.test('class',0)||p.test('let [',0)) return
+		let e = Expression.tryMatch(src,1,y,a,p);
+		if (e) return new ExpressionStatement(e.start,e.end,[e])
+	}
+}
+
+class ReturnStatement extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let bt = p.pos;
+		if (p.test('return')) {
+			
+			if (p.get()==';') return new ReturnStatement(bt,p.pos)
+			else {
+				p.goto(p.pos-1)
+				p.consumews()
+				let e = Expression.tryMatch(src,1,y,a,p)
+				if (e) {
+					if (p.get()==';') {
+						return new ReturnStatement(bt,e.end+1)
+					}
+				}
+			}
+		}
+		p.goto(bt)
+	}
+}
+class Expression extends ParseNode {
+	static tryMatch(src,i,y,a,p){
+		let e = AssignmentExpression.tryMatch(src,i,y,a,p)
+		if (e) return new Expression(e.start,e.end,[e])
+	}
+}
+class Declaration extends ParseNode {
+	static tryMatch(src, y, a, p) {
+		let hd = HoistableDeclaration.tryMatch(src,y,a,0,p);
+		if (hd) {
+			return new Declaration(hd.start, hd.end, [hd])
+		}
+		// todo other expressions.
+		let ld = LexicalDeclaration.tryMatch(src,1,y,a,p)
+		if (ld) {
+			let d = new Declaration(ld.start, ld.end)
+			d.children.push(ld)
+			return d
+		}
+	}
+}
+class HoistableDeclaration extends ParseNode {
+	static tryMatch(src,y,a,d,p) {
+		let f = FunctionDeclaration.tryMatch(src,y,a,d,p)
+		if (f) {
+			return new HoistableDeclaration(f.start,f.end,[f])
+		}
+	}
+}
+class FunctionDeclaration extends ParseNode {
+	static tryMatch(src,y,a,d,p) {
+		let bt = p.pos
+		if (p.test('function ')) {
+			let id = BindingIdentifier.tryMatch(src,y,a,p);
+			if (id) {
+				p.consumews()
+				if (p.get()=='(') {
+					p.consumews()
+					let params = FormalParameters.tryMatch(src,0,0,p)
+					if (params) {
+						p.consumews()
+						if (p.get()==')') {
+							p.consumews()
+							if (p.get()=='{'){
+								let fbody = FunctionBody.tryMatch(src,0,0,p)
+								if (fbody) {
+									p.consumews()
+									if (p.get() =='}') {
+										return new FunctionDeclaration(bt, p.pos, [id, params,body])
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+class FormalParameters extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let bt = p.pos
+		let rest = FunctionRestParameter.tryMatch(src,y,a,p)
+		if (rest) {
+			return new FormalParameters(rest.start,rest.end,[rest])
+		}
+		let params = FormalParameterList.tryMatch(src,y,a,p)
+		if (params) {
+			if (p.get() == ',') {
+				rest = FunctionRestParameter.tryMatch(src,y,a,p)
+				if (rest) {
+					return new FormalParameters(params.start,rest.end,[params,rest])
+				}
+				return new FormalParameters(params.start,params.end+1,[params])
+			}
+			else (p.goto(p.pos-1))
+			return new FormalParameters(params.start,params.end,[params])
+		}
+		return new FormalParameters(bt,bt)
+	}
+}
+
+class FormalParameterList extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let params = []
+		let f = 1
+		let i = FormalParameter.tryMatch(src,y,a,p)
+		if (!i) return
+		params.push(i)
+		while(f) {
+			if(p.get()!=',') {
+				f=0
+				p.goto(p.pos-2)
+				break
+			}
+			let i=FormalParameter.tryMatch(src,y,a,p)
+			if (!i) {
+				f=0
+				p.goto(p.pos-1)
+				break
+			}
+			params.push(i)
+		}
+		return new FormalParameterList(params[0].start,params[params.length-1].end,...params)
+	}
+}
+
+class FunctionRestParameter extends ParseNode{
+	static tryMatch(src,y,a,p) {
+
+	}
+}
+
+class FormalParameter extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let bt = p.pos
+		let param = BindingElement.tryMatch(src,y,a,p)
+		if (param) return new FormalParameter(param.start,param.end,[param])
+		else p.goto(bt)
+	}
+}
+class BindingElement extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let snb = SingleNameBinding.tryMatch(src,y,a,p)
+		if (snb) return new BindingElement(snb.start,snb.end,[snb])
+	}
+}
+class SingleNameBinding extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let id = BindingIdentifier.tryMatch(src,y,a,p)
+		if (!id) return
+		let init = Initializer.tryMatch(src,1,y,a,p)
+		if (init) return new SingleNameBinding(id.start,init.end,[id,init])
+		return new SingleNameBinding(id.start,id.end, [id])
+	}
+}
+class FunctionBody extends ParseNode {
+	static tryMatch(src,y,a,p) {
+		let sl = FunctionStatementList.tryMatch(src,y,a,p)
+		return new FunctionBody(sl.start,sl.end,[sl])
+	}
+}
+class FunctionStatementList extends ParseNode{
+	static tryMatch(src,y,a,p) {
+		let bckt = p.pos
+		let s = StatementList.tryMatch(src,y,a,1,p)
+		if (s) return new FunctionStatementList(s.start, s.end, [s])
+
+		return new FunctionStatementList(bckt,bckt)
+	}
+}
 function ScriptRecord() {
 	let record = {};
 	record.Realm = undefined
@@ -567,6 +820,19 @@ class Parser {
 			}
 		}
 	}
+
+	test(str, consumeIfTrue = true) {
+		let len = str.length
+		let p = 1
+		for (let i = 1; i-1<len; i++) {
+			let c = this.peek(i)
+			if (c != str.charAt(i-1)) return false
+		}
+		if (consumeIfTrue) {
+			this.goto(this.pos+len+1)
+		}
+		return true
+	}
 }
 
 const Tokens = {
@@ -576,23 +842,22 @@ const Tokens = {
 
 let source = `const a = 'Hello World!';
 function main(thing) {
-	console.log(thing);
 	return 0;
-};
+}
 let b;
 b = false;
 b = !b;
 if (b) {
 	main(a);
 }`
+dbg = !1
 let a = new Parser()
 let s = a.ParseScript(source)
-//console.dir(s, {depth:null})
+console.dir(s, {depth:null})
 function dbgtree(t) {
 	console.log(t.constructor.name + ': ' + source.substr(t.start,t.end-t.start))
 	t.children.forEach((c) => {
 		dbgtree(c)
 	})
 }
-
-dbgtree(s.ECMAScriptCode)
+//dbgtree(s.ECMAScriptCode)
